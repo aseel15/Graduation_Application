00f84f04664f93e199b88d9880b3be2faf04a2a5
aseel15 <aseel.zbai@gmail.com>
2022-04-13 02:38:25 +0300

    sec commit

---
+++ app/src/main/java/com/example/graduationapplication/TripMapActivity.java
@@ -0,0 +1,14 @@
+package com.example.graduationapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TripMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_map);
+    }
+}
