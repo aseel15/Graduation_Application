29e5e010807ce1640339f934f86441cec9675cee
aseel15 <aseel.zbai@gmail.com>
2022-04-13 03:08:44 +0300

    second commit

--- app/src/main/java/com/example/graduationapplication/TripMapActivity.java
+++ app/src/main/java/com/example/graduationapplication/TripMapActivity.java
@@ -3,9 +3,10 @@ package com.example.graduationapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TripMapActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
